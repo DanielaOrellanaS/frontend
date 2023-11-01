3b352d9fea042f7a3c09e5d163f3f54ae9ba3dd2
DanielaOrellanaS <deorellana@espe.edu.ec>
2023-11-01 09:33:06 -0500

    implement post login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import FormLogin from './Components/FormLogin'; 
 import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const navigate = useNavigate(); 
-  const redirectToAccounts = () => {
-    navigate('/accounts');
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('https://tradinapi.azurewebsites.net/authapi/login/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        // Almacenar el token de acceso en el almacenamiento local o en el estado de la aplicación
+        localStorage.setItem('accessToken', data.key);
+        navigate('/accounts');
+      } else {
+        // Manejar el caso en que el inicio de sesión falla
+        console.error('Inicio de sesión fallido');
+      }
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error);
+    }
   };
 
   return (
@@ -15,9 +41,15 @@ function App() {
         <img src="Logo.png" className="App-logo" alt="NeoLogo" style={{ width: '100px', height: '100px' }} />
         <div className="login-container">
           <img src="NEO.png" alt="NEO" style={{ width: '100px', height: '100px' }} />
-          <FormLogin />
+          <FormLogin
+            username={username}
+            password={password}
+            setUsername={setUsername}
+            setPassword={setPassword}
+          />
         </div>
-        <button className="custom-button" onClick={redirectToAccounts}>
+        <br/>
+        <button className="custom-button" onClick={handleLogin}>
           Iniciar Sesion
         </button>
         <button className="custom-button">
--- src/Components/FormLogin.js
+++ src/Components/FormLogin.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
-function FormLogin() {
-  return (
-    <div className="form-group custom-form">
-      <input type="email" id="email" name="email" placeholder="Email" className="custom-input" />
-      <input type="password" id="password" name="password" placeholder="Password" className="custom-input" />
-    </div>
-  );
-}
+  function FormLogin({ username, password, setUsername, setPassword }) {
+    const handleUsernameChange = (e) => {
+      setUsername(e.target.value);
+    };
 
-export default FormLogin;
+    const handlePasswordChange = (e) => {
+      setPassword(e.target.value);
+    };
+
+    return (
+      <div className="form-group custom-form">
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          className="custom-input"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          className="custom-input"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+    );
+  }
+
+  export default FormLogin;
