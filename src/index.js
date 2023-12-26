30c3cdbd26e3718b063c6500555658fa1ee78e9d
DanielaOrellanaS <deorellana@espe.edu.ec>
2023-12-25 21:53:31 -0500

    change reactDom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import './index.css';
 import App from './App';
@@ -11,20 +11,21 @@ import FormCreate from './Components/FormCreate';
 import Indicator from './Routes/Indicator';
 import EventDetail from './Components/EventDetail';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+
+const rootElement = (
   <Router>
-    <React.StrictMode>
-      <Routes>
-        <Route path="/" element={<App />} /> 
-        <Route path="/notices" element={<Notices />} />
-        <Route path="/indicator" element={<Indicator />} />
-        <Route path="/reports" element={<Reports />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/accounts" element={<Accounts />} />
-        <Route path="/createaccount" element={<FormCreate />} />
-        <Route path="/detalle-evento/:nameEvent" element={<EventDetail />} />
-      </Routes>
-    </React.StrictMode>
-  </Router>,
-  document.getElementById('root')
+    <Routes>
+      <Route path="/" element={<App />} /> 
+      <Route path="/notices" element={<Notices />} />
+      <Route path="/indicator" element={<Indicator />} />
+      <Route path="/reports" element={<Reports />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/accounts" element={<Accounts />} />
+      <Route path="/createaccount" element={<FormCreate />} />
+      <Route path="/detalle-evento/:nameEvent" element={<EventDetail />} />
+    </Routes>
+  </Router>
 );
+
+createRoot(root).render(rootElement);
